8cc948da940c8d38268a5d21ba7cc99ae6d767fe
Piyush <Piyush22122@gmail.com>
2024-07-20 11:39:52 +0530

    setup database

--- connectMongoos.js
+++ connectMongoos.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 function connectDB(url) {
-    return mongoose.connect(url).then(data => console.log("Connecting Database...")).catch(err => console.log("Error in connecting Database..."));
+    return mongoose.connect(url).then(data => console.log("Connecting Database...")).catch(err => console.log(`Error in connecting Database...${err}`));
 }
 export default connectDB;
