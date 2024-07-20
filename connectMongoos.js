e2bd6c0f0cc2daddf5c8b99b4bb2b8ce52941d8b
Piyush <Piyush22122@gmail.com>
2024-07-20 12:16:28 +0530

    changed contents

--- connectMongoos.js
+++ connectMongoos.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 function connectDB(url) {
-    return mongoose.connect(url).then(data => console.log("Connecting Database...")).catch(err => console.log(`Error in connecting Database...${err}`));
+    return mongoose.connect(url).then(data => console.log("Database connected...")).catch(err => console.log(`Error in connecting Database...${err}`));
 }
 export default connectDB;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';  // Import dotenv for environment variables
 dotenv.config();;
 import express, { urlencoded } from "express";
 const app = express();
+const localport = 8188;
 import connectDB from "./connectMongoos.js";
 import authrouts from "./routs/authRoutes.js";
 import pageroutes from "./routs/authRoutesPages.js";
@@ -14,6 +15,6 @@ app.set("view engine", "ejs");
 app.use("/pages", pageroutes);
 app.use("/auth", authrouts);
 app.use("/task", verifytokens, taskrouts);
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || localport, () => {
     console.log(`Starting Server at port: ${process.env.PORT}...`);
 })
